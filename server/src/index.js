f728b8ffbbffc8e59f507d11f82e7e4727116e51
SUJIT KUMAR PRAJAPATI <sujitkumar18081996@gmail.com>
2025-12-16 15:40:15 +0530

    Add server folder

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () =>
+      console.log("Server running on port", PORT)
+    );
+  })
+  .catch(err => console.log(err));
